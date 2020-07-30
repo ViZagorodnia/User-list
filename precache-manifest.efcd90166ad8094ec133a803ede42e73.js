820b01aec8bce1fe36589b9ccd9e6c805298fc11
ViZagorodnia <viktoriazagorodnia@gmail.com>
2020-07-30 12:28:25 +0300

    Updates

---
+++ precache-manifest.efcd90166ad8094ec133a803ede42e73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48d42b02f005bb3d50f516c3911808a",
+    "url": "/User-list/index.html"
+  },
+  {
+    "revision": "3a4be898e778eedf44e8",
+    "url": "/User-list/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "64469136418576037ddc",
+    "url": "/User-list/static/css/main.3a46bc9e.chunk.css"
+  },
+  {
+    "revision": "3a4be898e778eedf44e8",
+    "url": "/User-list/static/js/2.ccfb55a4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/User-list/static/js/2.ccfb55a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64469136418576037ddc",
+    "url": "/User-list/static/js/main.6445a3dc.chunk.js"
+  },
+  {
+    "revision": "93536a888c5b82dfe748",
+    "url": "/User-list/static/js/runtime-main.f36a372c.js"
+  }
+]);
